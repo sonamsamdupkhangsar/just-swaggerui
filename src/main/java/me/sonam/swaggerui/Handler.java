c20569c5baca8425f774b5f88cff6e9350eb9e1b
Sonam Samdupkhangsar <sonamsamdupkhangsar@Sonams-MacBook-Pro.local>
2022-12-29 16:21:14 -0700

    health check log at trace

--- src/main/java/me/sonam/swaggerui/Handler.java
+++ src/main/java/me/sonam/swaggerui/Handler.java
@@ -1,9 +1,6 @@
-package me.sonam.temp;
+package me.sonam.swaggerui;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
-import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Mono;
