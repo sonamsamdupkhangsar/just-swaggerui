4ad44accaa12810ad2bc04e3af212441929d9cf9
Sonam Samdupkhangsar <sonamsamdupkhangsar@Sonams-MacBook-Pro.local>
2022-12-30 16:51:06 -0700

    remove crossorigin annotation

--- src/main/java/me/sonam/Application.java
+++ src/main/java/me/sonam/Application.java
@@ -11,7 +11,6 @@ import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 import java.util.Arrays;
 
 @SpringBootApplication
-@CrossOrigin
 public class Application {
 
     public static void main(String[] args) {
